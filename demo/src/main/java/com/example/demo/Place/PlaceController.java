91462d97fcb7d5ecbcc1a98f5d69d1e7f881dc43
Tino Santeri Juhani Sipponen <tinosipponen@gmail.com>
2021-05-04 17:28:34 +0300

    Set URL to fetch all places

--- demo/src/main/java/com/example/demo/Place/PlaceController.java
+++ demo/src/main/java/com/example/demo/Place/PlaceController.java
@@ -2,13 +2,13 @@ package com.example.demo.Place;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "api/v1/places")
 public class PlaceController {
 
@@ -21,7 +21,7 @@ public class PlaceController {
     }
 
     @GetMapping
-    public List<Place> getPlaces() throws JsonProcessingException {
+    public Place getPlaces() throws JsonProcessingException {
         return placeService.getPlaces();
     }
 }
--- demo/src/main/java/com/example/demo/Place/PlaceService.java
+++ demo/src/main/java/com/example/demo/Place/PlaceService.java
@@ -1,21 +1,19 @@
 package com.example.demo.Place;
 
 import com.example.demo.Models.Data.Data;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.type.MapType;
-import com.fasterxml.jackson.databind.type.TypeFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.context.Theme;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -28,22 +26,20 @@ public class PlaceService {
     {
         this.restTemplate = builder.build();
         this.mapper = mapper;
-        //this.mapper.setPropertyNamingStrategy(PropertyNamingStrategy.LOWER_CAMEL_CASE);
+        this.mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
     }
-    public List<Place> getPlaces() throws JsonProcessingException {
+    public Place getPlaces() throws JsonProcessingException {
 
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(
-                "http://open-api.myhelsinki.fi/v1/places/?limit=1",
+                "http://open-api.myhelsinki.fi/v1/places/",
                 String.class);
         ObjectReader reader = mapper.readerFor(new TypeReference<List<Data>>() {
         });
         JsonNode root = mapper.readTree(responseEntity.getBody());
-        //var data = root.path("data").get(0);
 
         var placeObj = mapper.treeToValue(root, Place.class);
-        return List.of(
-                new Place()
-        );
+        return placeObj;
 
     }
 }
